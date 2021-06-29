a39d5084f10c3d8005e841631114485af6f5c7ab
Rodolfo S <rnsm14@gmail.com>
2021-06-29 11:13:51 +0200

    some components added

---
+++ src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fomulario = () => {
+    return ( 
+        <h1>Here is the Form</h1>
+     );
+}
+ 
+export default Fomulario;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return ( <header className="bg-alert">
+        <h1>Search Cocktails Recipes</h1>
+    </header> );
+}
+ 
+export default Header;
